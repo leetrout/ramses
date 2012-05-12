4a47924a5fc95c01fb8fb9d57f46d725dd4c1e93
Lee Trout <lee@leetrout.com>
2012-05-12 10:49:40 -0400

    Added team model.

--- django/ramses/stats/models.py
+++ django/ramses/stats/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Team(models.Model):
+    """A basic model that represents a sports team."""
+    name = models.CharField(max_length=100)
+    mascot = models.CharField(max_length=100, blank=True, null=True)

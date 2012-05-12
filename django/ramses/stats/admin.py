499f70590d644200a2f33094548e2b1bbeebdeff
Kathryn Faulkner <kfaulkner1@gmail.com>
2012-05-12 14:24:56 -0400

    Added slugify and changed order for Player.

--- django/ramses/stats/admin.py
+++ django/ramses/stats/admin.py
@@ -10,6 +10,14 @@ class TeamAdmin(admin.ModelAdmin):
     fields = ('name', 'slug', 'mascot')
 
 
+class PlayerAdmin(admin.ModelAdmin):
+    """Adds slugify options to player admin form."""
+    prepopulated_fields = {
+        "slug": ("last_name", "first_name",)
+    }
+    fields = ('team', 'first_name', 'last_name', 'slug')
+
+
 admin.site.register(Team, TeamAdmin)
-admin.site.register(Player)
+admin.site.register(Player, PlayerAdmin)
 admin.site.register(Sport)

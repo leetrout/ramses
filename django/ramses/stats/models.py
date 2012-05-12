e74644db6677724cb6c774834b5b86dedf7d8ffb
Steven King <cujophoto@gmail.com>
2012-05-12 11:00:07 -0400

    Added model for Player.

--- django/ramses/stats/models.py
+++ django/ramses/stats/models.py
@@ -4,3 +4,10 @@ class Team(models.Model):
     """A basic model that represents a sports team."""
     name = models.CharField(max_length=100)
     mascot = models.CharField(max_length=100, blank=True, null=True)
+    
+    
+class Player(models.Model):
+    """A basic modle that represents a player"""
+    last_name = models.CharField(max_length=100)
+    first_name = models.CharField(max_length=100)
+    team = models.ForeignKey("Team", related_name="players")

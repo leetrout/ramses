36bc29c08da30af96951f7b540f98f3666f1979b
Kathryn Faulkner <kfaulkner1@gmail.com>
2012-05-12 11:05:56 -0400

    Added the sports model and added slug field to other models

--- django/ramses/stats/models.py
+++ django/ramses/stats/models.py
@@ -4,10 +4,18 @@ class Team(models.Model):
     """A basic model that represents a sports team."""
     name = models.CharField(max_length=100)
     mascot = models.CharField(max_length=100, blank=True, null=True)
+    slug = models.SlugField()
     
     
 class Player(models.Model):
-    """A basic modle that represents a player"""
+    """A basic model that represents a player."""
     last_name = models.CharField(max_length=100)
     first_name = models.CharField(max_length=100)
     team = models.ForeignKey("Team", related_name="players")
+    slug = models.SlugField()
+
+
+class Sport(models.Model):
+    """A basic model that represents a sport."""
+    name = models.CharField(max_length=100)
+    slug = models.SlugField()
